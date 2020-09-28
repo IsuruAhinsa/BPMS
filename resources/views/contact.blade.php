0bd3aeff961dfa66ad87dee76dace1a9ff25ccb8
isuru <isurujayawickramat@gmail.com>
2020-09-28 17:04:49 +0530

    contact.blade.php update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,24 +13,7 @@
 
             <div class="row block-9">
                 <div class="col-md-7 order-md-last d-flex">
-                    <form action="#" class="bg-light p-4 p-md-5 contact-form">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Your Name">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Your Email">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Subject">
-                        </div>
-                        <div class="form-group">
-                            <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
-                        </div>
-                    </form>
-
+                    @include('forms.contact')
                 </div>
 
                 <div class="col-md-5 d-flex">
@@ -42,7 +25,7 @@
                                 </div>
                                 <div>
                                     <h3 class="mb-3">Address</h3>
-                                    <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+                                    <p>{!! setting('site.address') !!}</p>
                                 </div>
                             </div>
                         </div>
@@ -53,29 +36,29 @@
                                 </div>
                                 <div>
                                     <h3 class="mb-3">Contact Number</h3>
-                                    <p><a href="tel://1234567920">+ 1235 2355 98</a></p>
+                                    <p><a href="{{ setting('site.telephone') }}">{{ setting('site.telephone') }}</a></p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-12 ftco-animate">
                             <div class="box p-2 px-3 bg-light d-flex">
                                 <div class="icon mr-3">
-                                    <span class="icon-paper-plane"></span>
+                                    <span class="icon-envelope"></span>
                                 </div>
                                 <div>
                                     <h3 class="mb-3">Email Address</h3>
-                                    <p><a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+                                    <p><a href="mailto:{{ setting('site.email') }}">{{ setting('site.email') }}</a></p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-12 ftco-animate">
                             <div class="box p-2 px-3 bg-light d-flex">
                                 <div class="icon mr-3">
-                                    <span class="icon-globe"></span>
+                                    <span class="icon-school"></span>
                                 </div>
                                 <div>
-                                    <h3 class="mb-3">Website</h3>
-                                    <p><a href="#">yoursite.com</a></p>
+                                    <h3 class="mb-3">School</h3>
+                                    <p><a href="#">School Name</a></p>
                                 </div>
                             </div>
                         </div>
