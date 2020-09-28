133907f06812b72f953034af12310ac4fb4a04ba
isuru <isurujayawickramat@gmail.com>
2020-09-28 16:52:49 +0530

    Build Sliders

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Slider;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        return view('index', [
+            'sliders' => $this->getSliders(),
+        ]);
+    }
+
+    public function getSliders()
+    {
+        return Slider::orderBy('id', 'desc')->get();
     }
 }
---
+++ database/migrations/2020_09_28_105352_create_sliders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->text('image');
+            $table->string('vertical_text')->nullable();
+            $table->string('small_text')->nullable();
+            $table->string('heading')->nullable();
+            $table->string('paragraph')->nullable();
+            $table->string('btn_text')->nullable();
+            $table->string('btn_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
